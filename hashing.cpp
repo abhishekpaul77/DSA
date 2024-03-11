3f5af708a5ba6ffe5e6ce21c7c2c7afd55efcd44
Abhishek Paul <abhishekpaul5648@gmail.com>
2024-03-11 19:48:15 +0530

    hasing updated

--- hashing.cpp
+++ hashing.cpp
@@ -12,7 +12,6 @@ And boolean array can be declared of size 10^8 globally and it will not give seg
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int charhash(char any, string str)
 {
     int cnt = 0;
@@ -62,14 +61,46 @@ int main()
     // a-> Using for loop
     // string s = "abbc";
     // cout << charhash('b', s) << endl;
-    string s;
-    cout << "Enter the string" << endl;
-    cin >> s;
 
-    int hash[26] = {0};
-    for (int i = 0; i < s.size(); i++)
+    // b-> Using hash array
+    // string s;
+    // cout << "Enter the string" << endl;
+    // cin >> s;
+
+    // int hash[26] = {0};
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     hash[s[i]-'a']=hash[s[i]-'a']+1;//hash[s[i]-'a']++; or hash[s[i]-'a']+=1;
+    // }
+
+    // int query;
+    // cout << "Enter the number of queries" << endl;
+    // cin >> query;
+    // while (query--)
+    // {
+    //     char x;
+    //     cout << "Enter the element to be queried" << endl;
+    //     cin >> x;
+    //     cout << "The frequency of " << x << " is " << hash[x-'a'] << endl;
+    // }
+
+    // c-> Using map
+    int n;
+    cout << "Enter the size of array" << endl;
+    cin >> n;
+
+    int arr[n];
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
     {
-        hash[s[i]-'a']=hash[s[i]-'a']+1;//hash[s[i]-'a']++; or hash[s[i]-'a']+=1;
+        cout << "Enter the" << i << " element:" << endl;
+        cin >> arr[i];
+    }
+
+    map<int, int> hashmpp;
+    for (int i = 0; i < n; i++)
+    {
+        hashmpp[arr[i]]++;
     }
 
     int query;
@@ -77,11 +108,19 @@ int main()
     cin >> query;
     while (query--)
     {
-        char x;
+        int x;
         cout << "Enter the element to be queried" << endl;
         cin >> x;
-        cout << "The frequency of " << x << " is " << hash[x-'a'] << endl;
+        cout << "The frequency of " << x << " is " << hashmpp[x] << endl;
     }
+
+    // Time complexity of hashing is O(1) and space complexity is O(n)
+    // Time complexity of hashing using map is O(logn) and space complexity is O(n)
+    // Time complexity of hashing using unordered_map is O(1) and space complexity is O(n), for
+    // Worst case time complexity is O(n) and space complexity is O(n) because of collision.
+    // Collison is when two different keys are mapped to the same index of the array. To grab the theory of collision, refer to division method in hashing.
+    // To avoid collision we can use separate chaining or open addressing.
+    // Ordered list can have any data type as key and value but unordered_map can have only primitive data type as key.
 
     return 0;
 }
