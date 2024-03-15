17f79ce19f4fa9679fdfc33a47e43d4d1fbb842c
Abhishek Paul <abhishekpaul5648@gmail.com>
2024-03-15 19:45:33 +0530

    Merge sort added

---
+++ merge_sort.cpp
@@ -0,0 +1,78 @@
+// Merge sort
+// Time complexity: O(nlogn)
+// Space complexity: O(n)
+// About the algorithm: Merge Sort is a Divide and Conquer algorithm. It divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves. The merge() function is used for merging two halves. The merge(arr, l, m, r) is a key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &v, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (v[left] <= v[right])
+        {
+            temp.push_back(v[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(v[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(v[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.push_back(v[right]);
+        right++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        v[i] = temp[i - low];
+    }
+}
+
+void merge_sort(vector<int> &v, int low, int high)
+{
+    int mid = (low + high) / 2;
+    if (low == high)
+    {
+        return;
+    }
+    merge_sort(v, low, mid);
+    merge_sort(v, mid + 1, high);
+    merge(v, low, mid, high);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the element " << i + 1 << ": ";
+        cin >> v[i];
+    }
+
+    merge_sort(v, 0, n - 1);
+
+     cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
