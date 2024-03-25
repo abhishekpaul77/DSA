4e3a66ed636dd14198f716f893baab45dc9d490c
Abhishek Paul <abhishekpaul5648@gmail.com>
2024-03-25 19:57:35 +0530

    Merge sort updated

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,6 +3,8 @@
 // Space complexity: O(n)
 // About the algorithm: Merge Sort is a Divide and Conquer algorithm. It divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves. The merge() function is used for merging two halves. The merge(arr, l, m, r) is a key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one.
 
+// 
+
 
 
 #include <bits/stdc++.h>
