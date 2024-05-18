f47686fb1e305c2c47b64e1f9431d3f3f72b62f7
Abhishek Paul <abhishekpaul5648@gmail.com>
2024-05-18 13:22:22 +0530

    Count occurances

---
+++ count_occurances.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Brute force approach
+// Time complexity: O(n)
+// Space complexity: O(1)
+int count_BF(vector<int>& arr, int n, int x) {
+	// Write Your Code Here
+	int cnt=0;
+	for(int i=0;i<n;i++){
+		if(arr[i]==x){
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+// Binary search approach
+// Time complexity: 2*O(log2n)
+// Space complexity: O(1)
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            first = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            last = mid;
+            //look for larger index on the right
+            low = mid + 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return last;
+}
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+int count(vector<int>& arr, int n, int x) {
+	// Write Your Code Here
+	pair<int,int> ans=firstAndLastPosition(arr, n, x);
+	if(ans.first==-1) return 0;
+	else{
+		return ans.second-ans.first+1;
+	}
+}
+
+
+
+int main(){
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter the elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    return 0;
+}
