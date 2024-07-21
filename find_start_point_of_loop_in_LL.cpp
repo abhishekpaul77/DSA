b40e60cec9ae0ced78fb936b8c06059a32599899
Abhishek Paul <abhishekpaul5648@gmail.com>
2024-07-21 23:14:12 +0530

    Find start point of loop in LL

---
+++ find_start_point_of_loop_in_LL.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1){
+        data = data1;
+        next = NULL;
+    }
+
+};
+
+// Brute force approach: Hasing
+// Time complexity:O(N * 2 * log(N) )The algorithm traverses the linked list once, performing hashmap insertions and searches in the while loop for each node. The insertion and search operations in the unordered_map have a worst-case time complexity of O(log(N)). As the loop iterates through N nodes, the total time complexity is determined by the product of the traversal (O(N)) and the average-case complexity of the hashmap operations (insert and search), resulting in O(N * 2 * log(N)). 
+// Space complexity: O(n)
+Node* findStartNode_BF(Node* head){
+    unordered_map<Node*, int> map;
+    Node* temp = head;
+    while(temp != NULL){
+        if(map.find(temp) != map.end()){
+            return temp;
+        }
+        map[temp] = 1;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+// Optimized approach: Tortoise and Hare algorithm
+// Time complexity: O(N)
+// Space complexity: O(1)
+Node* findStartNode(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            slow=head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+
+int main(){
+    Node* head = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+    Node* fourth = new Node(4);
+    Node* fifth = new Node(5);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+     // Create a loop
+    fifth->next = third; 
+
+    // Check if there is a loop 
+    // n the linked list
+    if (findStartNode(head)) {
+        cout << "Loop detected in the linked list." << endl;
+    } else {
+        cout << "No loop detected in the linked list." << endl;
+    }
+
+    // Clean up memory (free the allocated nodes)
+    delete head;
+    delete second;
+    delete third;
+    delete fourth;
+    delete fifth;
+
+    return 0;
+}
